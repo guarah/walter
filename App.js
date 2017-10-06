7bb611c680fef3a8662ff3ab95516f3c8bddfbd8
Guaratã Alencar <guara_st@hotmail.com>
2017-10-06 01:40:28 -0300

    showing results

--- App.js
+++ App.js
@@ -1,44 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
-import { Slider } from 'nachos-ui' ;
+import { StyleSheet, Text, View, FlatList, TextInput, StatusBar, ScrollView } from 'react-native';
+import { Slider, Card } from 'nachos-ui' ;
 import * as firebase from 'firebase';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import {
+  RkTheme,
+  RkButton,
+  RkTextInput
+} 
+from 'react-native-ui-kitten';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: '', info: ''};
+    this.state = {text: '', info: '', results: []};
   }
 
   render() {
 
+    var listItems = this.state.results.map(function(item, i) {
+      return (
+        <Card
+          key={i}
+          footerContent={item.title || item.name}
+          image={'https://image.tmdb.org/t/p/w300/'+item.poster_path}
+        />
+      );
+    });
+
     return (
       <View style={styles.container}>
-        {/* Statusbar */}
-        <View style={styles.statusbar}/>
-        <View style={styles.navbar}>
-          <Text style={styles.navbarTitle}>{this.props.title}</Text>
-        </View>
+        <StatusBar
+          backgroundColor="red"
+          // barStyle="light-content"
+        />
 
-        <View style={{padding: 10}}>
-          <TextInput
-            style={{height: 40}}
-            placeholder="Search for movies or series..."
-            onSubmitEditing={(text) => {
-              this.setState({text})
-              search(text.nativeEvent.text).then(x => {
-                this.setState(previousState => {
-                  return { info: JSON.stringify(x.results[0]) };
-                });
-              });
-            }}
+        <RkTextInput 
+          label={<Icon name={'ios-search'}/>}
+          placeholder="Search for movies or series..."
+          style={{top: 20, height: 40, backgroundColor: 'white'}}
+          placeholder="Search for movies or series..."
+          onChangeText={(text) => {
+            this.setState({text})
+          }}
+          onSubmitEditing={(text) => {
+            this.setState({text: text.nativeEvent.text})
+            search(text.nativeEvent.text).then(x => {
+              this.setState({ results: x.results });
+            });
+          }}/>
+
+        <View style={styles.results}>
+          
+          <ScrollView contentContainerStyle={styles.contentContainer}>
+          {listItems}
+          
+          {/* <Card
+            footerContent='The avocado is a tree that is native to Mexico'
+            image='https://upx.cz/BsN'
           />
+          <Card
+            footerContent='Agaves are a type of succulent plant from Mexico'
+            image='https://upx.cz/BsD'
+          /> */}
+          </ScrollView>
         </View>
-
-        <View style={styles.navbar}>
-          <Text>{this.state.info}</Text>
-        </View>
-
-
 
       </View>
     );
@@ -62,20 +89,17 @@ function search(searchText) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 30
+    paddingHorizontal: 10,
   },
-  statusbar: {
-    backgroundColor: 'green'
+  results: {
+    flex: 2,
   },
-  navbar: {
-    backgroundColor: 'blue'
-  },
-  navbarTitle: {
-    backgroundColor: 'white'
-  },
+  contentContainer: {
+    paddingVertical: 20
+  }
 });
 
 // Initialize Firebase
