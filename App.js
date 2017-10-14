fcbcfe0044f4633e9811521f3b5e6bc21294c398
Guaratã Alencar <guara_st@hotmail.com>
2017-10-14 14:44:47 -0300

    basic structure - wip

--- App.js
+++ App.js
@@ -1,113 +1,53 @@
 import React from 'react';
 import Radium from 'radium';
-import { Text, View, TextInput, StatusBar, ScrollView } from 'react-native';
-import { Card } from 'nachos-ui' ;
-import { API_TMDB_KEY, FIREBASE_CONFIG } from './constants';
+
+import { View, StatusBar } from 'react-native';
+import { Header } from 'react-native-elements';
+import { TabNavigator } from "react-navigation";
+
+// components
+import Movie from './src/Movie/Movie.js'
+import Serie from './src/Serie/Serie.js';
+import SearchView from './src/search/SearchView.js';
+
+import { API_TMDB_KEY, FIREBASE_CONFIG } from './src/lib/constants';
 import * as firebase from 'firebase';
 const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
-
-const colors = {
-  white: {
-    color: 'white'
-  },
-  black: {
-    color: 'black'
-  }
-};
 
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#35343B',
     justifyContent: 'center',
-    paddingTop: 20,
-  },
-  srachBarContainerStyle: {
-    width: '92%',
-    height: 35,
-    marginLeft: 15,
-    marginTop: 8,
-    marginBottom: 10,
-    padding: 5,
-    borderRadius: 8,
-    backgroundColor: '#3d3d3d',
-    color: 'grey',
-    fontSize: 20,
-    fontStyle: 'italic',
-  },
-  results: {
-    flex: 2
-  },
-  contentContainer: {
-    paddingVertical: 20
-  },
-  headerText: {
-    fontSize: 30,
-    marginTop: 20,
-    marginLeft: 15,
-    fontWeight: 'bold',
-  },
-  colors: colors
+    paddingTop: 20
+  }
 };
 
-export default class App extends React.Component {
+const AppNavigator = TabNavigator({
+  // Movies: { screen: Movie },
+  Serie: { screen: Serie },
+});
+
+export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: '', info: '', results: []};
   }
 
   render() {
-    const listItems = this.state.results.map(function(item, i) {
-      return (
-        <Card
-          key={i}
-          footerContent={item.title || item.name}
-          image={'https://image.tmdb.org/t/p/w300/'+item.poster_path}
-        />
-      );
-    });
-
-    function search(searchText) {
-      const query = searchText.replace('','+');
-      return fetch(`https://api.themoviedb.org/3/search/multi?page=1&api_key=${API_TMDB_KEY}&query=${query}`)
-        .then((response) => response.json())
-        .then((responseJson) => {
-          return responseJson;;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
+    
 
     return (
       <View style={styles.container}>
+        
+        <StatusBar barStyle="light-content" backgroundColor="#222126" />
 
-        <StatusBar barStyle="light-content" />
-
-        <Text style={[styles.colors.white, styles.headerText]}>Search</Text>
-
-        <TextInput
-          placeholder='Series or movies...'
-          placeholderTextColor="grey"
-          style={[
-            styles.srachBarContainerStyle
-          ]}
-          onChangeText={(text) => {
-            this.setState({text})
-          }}
-          onSubmitEditing={(text) => {
-            this.setState({text: text.nativeEvent.text})
-            search(text.nativeEvent.text).then(x => {
-              this.setState({ results: x.results });
-            });
-          }}
+        <Header style={{backgroundColor: '#222126', height: 50, padding: 10}}n
+          leftComponent={{ icon: 'menu', color: '#BF3340' }}
+          centerComponent={{ text: 'Walter', style: { color: '#fff' } }} 
+          rightComponent={{ icon: 'home', color: '#BF3340' }}
         />
 
-        <View style={styles.results}>
-          <ScrollView contentContainerStyle={styles.contentContainer}>
-            {listItems}
-          </ScrollView>
-        </View>
+        <SearchView/>
 
       </View>
     );
